346723c5387169375907608f27b2b9a75e878aaa
leda84 <laura.dejesus929@gmail.com>
2022-02-03 23:58:23 -0600

    console log edit

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -31,5 +31,5 @@ const binder = {
 };
 
 console.log("The binder object:", binder)
-console.log("The sheet quantity:", binder["sheets"]["quantity"])
+console.log("The binder sheet quantity:", binder["sheets"]["quantity"])
 
